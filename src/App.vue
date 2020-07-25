5e519daa2a2c237f8192eb2bbe520944adbb03b7
Bukovski <kmanankov@bk.ru>
2020-07-25 11:43:59 +0300

    fix: style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
+<!--  <div id="app" :style="bgStyle">-->
   <div id="app">
     <my-header></my-header>
-
+    
     <div class="container">
       <loader v-if="loading"/>
       
@@ -15,14 +16,20 @@
 <script>
   import MyHeader from './components/Header.vue';
   import Loader from './components/Loader.vue';
-
-
+  
+  
   export default {
     name: 'app',
     components: { MyHeader, Loader },
     computed: {
       loading () {
         return this.$store.getters.loading
+      },
+      bgStyle() {
+        return {
+          'background': `url(${ require('@/assets/images/sign.jpg') }) repeat`,
+          'background-size': '15% auto'
+        }
       }
     },
     async created () {
@@ -34,49 +41,61 @@
 </script>
 
 <style>
-#app {
-  margin-top: 87px;
-  
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-
-.fade-enter-active {
-  animation: slideIn 0.5s;
-}
-
-.fade-leave-active {
-  animation: slideOut 0.5s forwards;
-}
-
-@keyframes slideIn {
-  from { transform: translateX(-100vw); }
-  to { transform: translateX(0px); }
-}
-
-@keyframes slideOut {
-  from { transform: translateX(0px); }
-  to { transform: translateX(100vw); }
-}
-
-/* Form.vue input phone number */
-.phone-wrapper {
-  position: relative;
-}
-
-.phone-wrapper .phone-flag {
-  position: absolute !important;
-  top: 0;
-  right: 10px;
-}
-
-@media (max-width: 768px) {
   #app {
-    margin-top: 55px;
+    position: relative;
+    margin-top: 87px;
+    height: 100vh;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
   }
-}
+  #app:before {
+    content: '';
+    position: absolute;
+    right: 0;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    background: url('./assets/images/sign.jpg') repeat;
+    background-size: 15% auto;
+    opacity: .1;
+  }
+  
+  
+  .fade-enter-active {
+    animation: slideIn 0.5s;
+  }
+  
+  .fade-leave-active {
+    animation: slideOut 0.5s forwards;
+  }
+  
+  @keyframes slideIn {
+    from { transform: translateX(-100vw); }
+    to { transform: translateX(0px); }
+  }
+  
+  @keyframes slideOut {
+    from { transform: translateX(0px); }
+    to { transform: translateX(100vw); }
+  }
+  
+  /* Form.vue input phone number */
+  .phone-wrapper {
+    position: relative;
+  }
+  
+  .phone-wrapper .phone-flag {
+    position: absolute !important;
+    top: 0;
+    right: 10px;
+  }
+  
+  @media (max-width: 768px) {
+    #app {
+      margin-top: 55px;
+    }
+  }
 </style>
